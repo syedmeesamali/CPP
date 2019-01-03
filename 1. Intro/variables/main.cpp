eec2ff90d47c806a2c28468db5f4d7eda8eda106
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-03 21:50:40 +0400

    Some practice with variables and strings in C++

--- 1. Intro/variables/main.cpp
+++ 1. Intro/variables/main.cpp
@@ -11,11 +11,17 @@ int main()
     cout <<"Char t4 has value: " << t4 <<endl;
     string new1 = "First string";
     cout <<"String new1 has value: " << new1 <<endl;
-
+    /*
     string name1 = "";
     cout <<"Please enter your name: ";
     cin >> name1;
     cout<< "Your name is: " << name1 <<endl;
-
+    */
+    string name1 = "";
+    getline(cin, name1);
+    cout <<"Hello how are you Mr.  "<<name1 <<endl;
+    string s1 = "Mar";
+    s1 += "ty";
+    cout <<"Concatenation of strings: "<< s1 <<endl;
     return 0;
 }

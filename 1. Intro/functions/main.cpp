f0de70f2a47c7ac88df20941e7231e01756619e7
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-19 16:06:06 +0400

    Some more classic functions in C++ - Return is a must for functions in C++

--- 1. Intro/functions/main.cpp
+++ 1. Intro/functions/main.cpp
@@ -11,10 +11,25 @@ int returnFive() {
     return 5;
 }
 
+int multiply(int a, int b) {
+    return a * b;
+}
+
+string createGreeting(string name) {
+    return "Greetings " + name;
+}
+
 int main() {
     test();
     test();
     test();
     cout << returnFive() << endl;;
+    cout << multiply (5,9) << endl;
+    string name;
+    cout <<"Enter some name for greetings!" <<endl;
+    cin >> name;
+    string greetString = createGreeting(name);
+    cout << greetString;
+
     return 0;
 }

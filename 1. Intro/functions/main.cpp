70fbd7aeab200c1c6ffe5e6de5718864d0678a6b
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-25 20:11:27 +0400

    Circle area function

--- 1. Intro/functions/main.cpp
+++ 1. Intro/functions/main.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 
+int circleArea(double r) {
+    return 3.141 * r * r;
+}
+
 int main() {
     int num = -5;
     int i = 0;
+    int rad=0;
     for (i = 0; i<=100; i++) {
         if (i % 2 == 0) {
             cout <<"Even number : "<<i <<endl;
         }
     }
+    cout << "Enter radius to calculate area of circle:" <<endl;
+    cin >> rad;
+    cout << "Circle area is: " <<circleArea(rad);
     return 0;
 }

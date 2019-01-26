b8846d84de0e0ab3338adc620e61c7a561d5a6f7
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-26 16:33:17 +0400

    Now swap function in (C++) works due to the "&" letter. Reference v/s pointers.

--- 1. Intro/functions/main.cpp
+++ 1. Intro/functions/main.cpp
@@ -2,14 +2,20 @@
 
 using namespace std;
 
-int circleArea(double r) {
+double circleArea(double r) {
     return 3.141 * r * r;
 }
 
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main() {
-    int num = -5;
     int i = 0;
     int rad=0;
+    int a = 15; int b = 35;
     for (i = 0; i<=100; i++) {
         if (i % 2 == 0) {
             cout <<"Even number : "<<i <<endl;
@@ -18,5 +24,8 @@ int main() {
     cout << "Enter radius to calculate area of circle:" <<endl;
     cin >> rad;
     cout << "Circle area is: " <<circleArea(rad);
+    swap(a,b);
+    cout <<"Swapped values of a and b: "<<a <<" and " <<b <<endl;
+
     return 0;
 }

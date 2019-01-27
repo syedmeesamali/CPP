5bdb4662633df7137cfe7a7ba7a3beec019bf2be
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-27 14:20:36 +0400

    Swap1 doesn't work due to direct reference and Swap2 works with "&" indirect reference

--- 1. Intro/functions/main.cpp
+++ 1. Intro/functions/main.cpp
@@ -2,30 +2,23 @@
 
 using namespace std;
 
-double circleArea(double r) {
-    return 3.141 * r * r;
+void swap1(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
 }
 
-void swap(int& x, int& y) {
+void swap2(int& x, int& y) {
     int temp = x;
     x = y;
     y = temp;
 }
 
 int main() {
-    int i = 0;
-    int rad=0;
     int a = 15; int b = 35;
-    for (i = 0; i<=100; i++) {
-        if (i % 2 == 0) {
-            cout <<"Even number : "<<i <<endl;
-        }
-    }
-    cout << "Enter radius to calculate area of circle:" <<endl;
-    cin >> rad;
-    cout << "Circle area is: " <<circleArea(rad);
-    swap(a,b);
-    cout <<"Swapped values of a and b: "<<a <<" and " <<b <<endl;
-
+    swap1(a,b);
+    cout <<"Swap1: values of a and b: "<<a <<" and " <<b <<endl;
+    swap2(a,b);
+    cout <<"Swap1: values of a and b: "<<a <<" and " <<b <<endl;
     return 0;
 }

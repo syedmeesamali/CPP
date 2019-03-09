22b87ebc821fccce0abb97b4a913593e5c331773
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-09 23:22:01 +0400

    Excellent example of nested STRUCTS

--- 3_Structs/structs1/main.cpp
+++ 3_Structs/structs1/main.cpp
@@ -9,17 +9,35 @@ struct person
     string gender;
 };
 
+struct Marriage
+{
+    person guy;
+    person girl;
+    int age;
+    int people;
+};
+
 void printinfo(person p)
 {
-    cout <<"Name is: "<<p.name<<" Gender is: "<<p.gender<<" and age is: "<<p.age;
+    cout <<"Name is: "<<p.name<<" Gender is: "<<p.gender<<" and age is: "<<p.age <<endl;
 }
+
+void printMarriageInfo(Marriage m)
+{
+    cout <<m.guy.name<<" got married to >> "<<m.girl.name<<" for "<<m.age <<" years!" <<endl;
+}
+
 
 int main()
 {
     person ali;
+    person mary = {"mary", 30, "female"};
     ali.name = "Syed Meesam Ali";
     ali.age = 32;
     ali.gender = "Male";
     printinfo(ali);
+    Marriage marriage = {ali, mary, 10, 200};
+
+    printMarriageInfo(marriage);
     return 0;
 }

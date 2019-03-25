a704df6801574a8ecb4ad85465edf16b947f969b
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-25 23:54:24 +0400

    Completion of class assignment

--- 3_Structs/structs2/main.cpp
+++ 3_Structs/structs2/main.cpp
@@ -11,9 +11,18 @@ public:
     //Below is a constructor for class person
     person(string a, int b, string c)
     {
+        cout << "Constructing object !" <<endl;
         name = a;
         age = b;
         gender = c;
+    }
+
+    person()
+    {
+        cout <<"No default arguments"<<endl;
+        name = "no default name";
+        age = 99;
+        gender = "undefined";
     }
 
     void printInfo()
@@ -29,34 +38,7 @@ int main()
 {
     person ali("Meesam", 31, "male");
     ali.printInfo();
+    person shah = person();
+    shah.printInfo();
     return 0;
 }
-
-//Below code for comparison only
-/*
-struct person
-{
-    string name;
-    int age;
-    string gender;
-};
-
-struct Marriage
-{
-    person guy;
-    person girl;
-    int age;
-    int people;
-};
-
-void printinfo(person p)
-{
-    cout <<"Name is: "<<p.name<<" Gender is: "<<p.gender<<" and age is: "<<p.age <<endl;
-}
-
-void printMarriageInfo(Marriage m)
-{
-    cout <<m.guy.name<<" got married to >> "<<m.girl.name<<" for "<<m.age <<" years!" <<endl;
-}
-
-*/

97f6709eb30478cce696a0e89f568c6a08a2f2c5
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-14 13:15:39 +0400

    Usage of enum with switch case statements

--- 3_Structs/enum/main.cpp
+++ 3_Structs/enum/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 using namespace std;
 
-enum weekDays {SA, SU, MO, TU, WE, TH, FR};
+enum weekDays {SA, SU, MO = 1, TU, WE, TH, FR};
+string getDay(weekDays);
+
 int main()
 {
     weekDays d = SU;
     cout <<"Enum SU is: " << d << endl;
     return 0;
+}
+
+string getDay(weekDays d)
+{
+    switch(d)
+    {
+    case MO:
+        return "Monday";
+    case TU:
+        return "Tuesday";
+    case WE:
+        return "Wednesday";
+    case TH:
+        return "Thursday";
+    case FR:
+        return "Friday";
+    case SA:
+        return "Saturday";
+    case SU:
+        return "Sunday";
+    }
+
 }

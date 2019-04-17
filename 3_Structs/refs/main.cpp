4b9717568d8d1f68dc859324c2f7f00a7f01fd55
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-17 23:55:54 +0400

    reference variables

--- 3_Structs/refs/main.cpp
+++ 3_Structs/refs/main.cpp
@@ -6,7 +6,13 @@ int main()
     string name = "Ali";
 
     cout <<"Name is : "<< name <<" and address is: " << &name << endl;
+    //Reference variable must always be initialized
     string &no_1 = name;
     cout <<"Name is : "<< no_1 <<" and address is: " << &no_1 << endl;
+
+    no_1 = "Meesam";
+    cout <<"Name is : "<< name <<" and address is: " << &name << endl;
+    cout <<"Name is : "<< no_1 <<" and address is: " << &no_1 << endl;
+
     return 0;
 }

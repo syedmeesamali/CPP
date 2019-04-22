7ff7ade7128858ad33993da381e0067530a248bf
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-22 22:16:52 +0400

    Pointers

--- 4_Pointers/pointers1/main.cpp
+++ 4_Pointers/pointers1/main.cpp
@@ -7,6 +7,10 @@ int main()
     int var = 5;
 
     cout << &var << endl;
-    int *p;
+
+    int *p; //* means that this variable is a POINTER only
+    p = &var;
+    cout << *p << endl; //This star helps to retrieve the value POINTED at by variable p
+
     return 0;
 }

37cad8a058d6be4a7ffb3ae229ee0071a1537a8e
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-26 17:43:25 +0400

    Pointers review

--- 4_Pointers/pointers1/main.cpp
+++ 4_Pointers/pointers1/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
     int var = 5;
-
     cout << &var << endl;
 
-    int *p; //* means that this variable is a POINTER only
-    p = &var;
-    cout << *p << endl; //This star helps to retrieve the value POINTED at by variable p
+    int *p; // * means that this variable is a POINTER only
+    p = &var; //So now in this pointer variable p we can store the ADDRESS of variable 'var'
+
+    cout << p << endl; //This star helps to retrieve the value POINTED at by pointer variable p
 
     return 0;
 }

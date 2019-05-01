49c4f68a80d31d1ad658eccf3ee2248591d459a7
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-01 18:46:54 +0400

    Pointers study and understanding

--- 4_Pointers/pointers1/main.cpp
+++ 4_Pointers/pointers1/main.cpp
@@ -10,7 +10,7 @@ int main()
     int *p; // * means that this variable is a POINTER only
     p = &var; //So now in this pointer variable p we can store the ADDRESS of variable 'var'
 
-    cout << p << endl; //This star helps to retrieve the value POINTED at by pointer variable p
+    cout << *p << endl; //This star helps to retrieve the value POINTED at by pointer variable p
 
     return 0;
 }

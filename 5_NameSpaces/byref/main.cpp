83a303d9cf7e8dd0d687c04b6ff9add3cd6c847b
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-13 17:06:36 +0400

    Value passed with reference changes original variable

--- 5_NameSpaces/byref/main.cpp
+++ 5_NameSpaces/byref/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void increase(int a)
+void increase(int& a) //Due to passing value as reference now the original value also will change
 {
     a++;
     cout<<"Value of a: " << a << endl;
@@ -10,8 +10,10 @@ void increase(int a)
 
 int main()
 {
+    //arguments passed by value don't change original
     int testNo = 5;
     increase(testNo);
+
     cout << "Test no. after increment: " << testNo << endl;
     return 0;
 }

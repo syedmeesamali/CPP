675339a8db19fe3c4ee8128b62a3cb45ba0ac492
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-16 12:56:00 +0400

    Some C refresher work

--- 5_NameSpaces/Ext_Files/simple_c/main.c
+++ 5_NameSpaces/Ext_Files/simple_c/main.c
@@ -3,9 +3,15 @@
 
 int main()
 {
-    char c, d;
-    c = 'd';
-    d = c;
-    printf("d = %c\n",d);
+    int a = 0;
+    while (a < 5)
+    {
+        a++;
+        if (a == 3)
+        {
+            printf("Inside if loop \n");
+        }
+        printf("a = %d \n",a);
+    }
     return 0;
 }

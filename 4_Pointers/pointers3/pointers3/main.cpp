72c4e3b017b01a0a94b7d23689276aa73927c2cd
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-23 15:45:36 +0400

    array storage and pointers

--- 4_Pointers/pointers3/pointers3/main.cpp
+++ 4_Pointers/pointers3/pointers3/main.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int main()
 {
     int a[3];
-    short int zm;
+
     int *p = &a[0];
     cout << a << endl;
     cout << &a[0] << endl;  //Name of an array is also a pointer
-    cout << p << endl;
+    cout << &a[1] << endl;
+    cout << &a[2] << endl;
+    cout <<endl << endl <<endl;
+    cout << a << endl;
+    cout << a+1 << endl;
+    cout << a+2 << endl;
 }

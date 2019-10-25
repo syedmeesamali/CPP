0a53ec28cbee16c64c817d7d37879038c2c84b33
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-25 22:24:00 +0400

    reviewing C++ stuff

--- 5_NameSpaces/GetPoint/getpoint/main.cpp
+++ 5_NameSpaces/GetPoint/getpoint/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+/*
 void OperationOnPoints();
 template<typename t, typename t2>
 t add(t var1, t var2)
@@ -7,10 +7,31 @@ t add(t var1, t var2)
     return var1 + var2;
 }
 
+template<>
+Point2D add(Point2D var1, Point2D var2)
+{
+    Point2D temp;
+    temp.setX(var1.getX + var2.getX);
+
+}
+
+*/
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string text = "meesam";
+    for (int i=0; i<text.length(); i++)
+    {
+        cout << text[i] << endl;
+    }
+    char ch = text[0];
+    cout << "char is: " << ch << endl;
     return 0;
 }
+/*
+void OperationOnPoints()
+{
+    Point2D
+}
+*/

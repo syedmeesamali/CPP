588ccd3ee18d8db637cd4849523af0e5aa77ade1
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-27 12:52:33 +0400

    some more efforts but stuck badly

--- 5_NameSpaces/GetPoint/getpoint/main.cpp
+++ 5_NameSpaces/GetPoint/getpoint/main.cpp
@@ -1,66 +1,28 @@
 #include <iostream>
-/*void OperationOnPoints();
-template<typename t, typename t2>
-t add(t var1, t var2)
-{
-    return var1 + var2;
-}
-template<>
-Point2D add(Point2D var1, Point2D var2)
-{
-    Point2D temp;
-    temp.setX(var1.getX + var2.getX);
-
-} */
 using namespace std;
 int main()
 {
     int nums[] = {7, 5, 9, 8, 6, 15, 23, 29, 40, 35};
     int Size = sizeof(nums) / sizeof(int);
-    cout <<"Size is: " << Size << endl;
-    int x = 0;
-    int i = 0;
-    int y = 0;
-    int counter = 0;
-    int tmp;
-    /*while (y < Size )
-    {
-        cout << "Nums[" << y << "] is: " << nums[y] << endl;
-        y++;
-    }
-    cout <<"Trying to sort below Size is: " << Size << endl;
-    */
-    do
-    {
-        while (x < Size)
-        {
+    int x = 0, i = 0, counter = 0, tmp = 0;
+    do { while (x < Size)
+        {   tmp = nums[x];
             if (nums[x] > nums[x+1])
-            cout <<"Counter is now: " << counter << endl;
-            counter = counter++ ;
-            {
-                tmp = nums[x];
+            cout <<"Counter ------> " << counter << endl;
+            {   counter = counter + 1;
                 nums[x] = nums[x+1];
                 nums[x+1] = tmp;
-                counter = counter - 1; //Decrease count for every good shuffle
             }
-            cout << "Nums[" << x << "] is: " << nums[x] << endl;
+            cout << "Nums[" << x << "] = " << nums[x] << endl;
             x++;
-        } cout <<"Counter now: " << counter << endl;
-    } while (counter > 0); //Check for the shuffle iterations
-
-    cout <<"New loop below! i is now: " << i << endl;
+        }
+        counter = counter - 1;
+    } while (counter > 0);
+    cout <<"After shuffle... " << endl;
     int Size1 = sizeof(nums) / sizeof(int);
-    cout <<"Size1 is: " << Size1 << endl;
     while (i < Size1 )
-    {
-        cout << "Nums[" << i << "] is: " << nums[i] << endl;
+    {   cout << "Nums[" << i << "] = " << nums[i] << endl;
         i++;
     }
     return 0;
 }
-/*
-void OperationOnPoints()
-{
-    Point2D
-}
-*/

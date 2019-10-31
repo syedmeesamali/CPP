c2efadbc74c441d12301b25ef3f1260c257d2079
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-31 10:55:48 +0400

    bubble sort completed successfully

--- 5_NameSpaces/GetPoint/getpoint/main.cpp
+++ 5_NameSpaces/GetPoint/getpoint/main.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
-
-//Swap if one is bigger than other
 void swap(int* x, int* y)
 {
     int* tmp = x;
     *x = *y;
     *y = *tmp;
 }
-
 using namespace std;
 int main()
 {
-    int nums[] = {7, 5, 9, 8, 6, 15, 29, 23, 40, 35};
+    int nums[] = {7, 5, 9, 8, 39, 15, 29, 23, 40, 35};
     int Size = sizeof(nums) / sizeof(int);
-    cout <<"Size is: " << Size << endl;
-    int x = 0; int i = 0; int j = 0; int k = 0; int swaps = 0;
-    for (j=0; j<Size; j++)
+    int x = 0; int i = 0; int j = 0; int k = 0;
+    for (j=0; j<Size-1; j++)
     {
-        for (k=0; k<Size-j; k++)
-        {
-            swaps = 0;
-            if (nums[k] > nums[k+1])
-            {
+        for (k=0; k<Size-j-1; k++)
+        {    if (nums[k] > nums[k+1])
                 swap(nums[k], nums[k+1]);
-                cout << "Nums[" << k << "] is: " << nums[k] << endl;
-                swaps++;
-            } //End of if
-        } //End of inner for loop
-        if (swaps == 0)  {   break;   }
-    } //End of outer for loop
-
-    cout <<"New loop below! i is now: " << i << endl;
+        }
+    }
+    cout <<"Sorted list below!"<< endl;
     int Size1 = sizeof(nums) / sizeof(int);
     while (i < Size1 )
     {   cout << "Nums[" << i << "] = " << nums[i] << endl;
